62d4cb421ff94327edfc608406fd7c4880faae6a
minitong <381520993@qq.com>
2018-09-10 17:57:46 +0800

    chore

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
-﻿var api = "http://192.168.1.100:3000/api/";
+﻿//var api = "http://192.168.1.100:3000/api/";
+var api = "https://www.dcar.xyz/api/";
 var ethProvider = 'HTTP://127.0.0.1:7545';
 //var iapi = "http://youpiao.zhengdazikaowang.net";
 //var img_url = "http://youpiao.zhengdazikaowang.net/"
